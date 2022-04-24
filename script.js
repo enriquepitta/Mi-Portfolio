50edfe24e3a9d087ad1d883359d0b9ec9e249de5
Enrique <enriquearielpitta@gmail.com>
2022-04-24 15:45:02 -0400

    Cambio del switch dark/light mode

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 // OBTENER ELEMENTOS ATRAVEZ DE SU ID
 const toggleTheme = document.getElementById("toggle-theme")
-const toggleIcon = document.getElementById("toggle-icon")
 const toggleText = document.getElementById("toggle-text")
 const toggleColors = document.getElementById("toggle-colors")
 const rootStyles = document.documentElement.style
-// CUANDO ESCUCHA EL CLICK CAMBIAR LA CLASE AL BODY
-toggleTheme.addEventListener("click", () => {
-    document.body.classList.toggle("dark")
-    if (toggleIcon.src.includes("moon.svg")) {
-        toggleIcon.src = "assets/icons/sun.svg"
-        toggleText.textContent = "Light Mode"
-    } else {
-        toggleIcon.src = "assets/icons/moon.svg"
-        toggleText.textContent = "Dark Mode"
+
+toggleColors.addEventListener('click', (e) => {
+    if (e.target.classList[0] === 'colors__item') {
+      rootStyles.setProperty('--primary-color', e.target.dataset.color)
     }
 })
 
-toggleColors.addEventListener("click", (e) => {
-    rootStyles.setProperty("--primary-color", e.target.dataset.color)
+// CUANDO ESCUCHA EL CLICK CAMBIAR LA CLASE AL BODY Y AL TOGGLE
+toggleTheme.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    toggleTheme.classList.toggle("active")
+    toggleText.textContent = (toggleText.textContent == "Dark Mode" ? "Light Mode" : "Dark mode")
 })
