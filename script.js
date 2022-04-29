ca4b30fe8598aa410e2f820b7ab0d9b8d6179848
Enrique <enriquearielpitta@gmail.com>
2022-04-29 16:24:25 -0400

    Solucionado toggle language

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // OBTENER ELEMENTOS ATRAVEZ DE SU ID
 const toggleTheme = document.getElementById("toggle-theme")
-const togleLanguage = document.getElementById("toggle-language")
-const togleLanguageText = document.getElementById("english")
+const toggleLanguage = document.getElementById("toggle-language")
+const toggleLanguageText = document.getElementById("english")
 
 const toggleText = document.getElementById("toggle-text")
 const toggleColors = document.getElementById("toggle-colors")
 const rootStyles = document.documentElement.style
 
 toggleColors.addEventListener('click', (e) => {
-    if (e.target.classList[0] === 'colors__item') {
-      rootStyles.setProperty('--primary-color', e.target.dataset.color)
-    }
+  if (e.target.classList[0] === 'colors__item') {
+    rootStyles.setProperty('--primary-color', e.target.dataset.color)
+  }
 })
-  
+
 // CUANDO ESCUCHA EL CLICK CAMBIAR LA CLASE AL BODY Y AL TOGGLE
 toggleTheme.addEventListener("click", () => {
   document.body.classList.toggle("dark")
@@ -20,14 +20,13 @@ toggleTheme.addEventListener("click", () => {
   toggleText.textContent = (toggleText.textContent == "Dark" ? "Light" : "Dark")
 })
 
-togleLanguage.addEventListener("click", (e) => { 
-  togleLanguage.classList.toggle("active")
-  togleLanguageText.textContent = (togleLanguageText.textContent == "English" ? "Español" : "English")
+toggleLanguage.addEventListener("click", (e) => {
+  toggleLanguage.classList.toggle("active")
+  toggleLanguageText.textContent = (toggleLanguageText.textContent == "English" ? "Español" : "English")
 })
 
-togleLanguage.addEventListener("click", (e) => {
-  e.target.parentElement.dataset.language = (e.target.dataset.language == "en" ? "es" : "en")
-  e.target.dataset.language = e.target.parentElement.dataset.language 
-  // e.target.dataset.language
-  console.log(e.target.parentElement.dataset.language);
+toggleLanguage.addEventListener("click", (e) => {
+  toggleLanguage.dataset.language = (toggleLanguage.dataset.language == "en" ? "es" : "en")
+  
+  console.log(toggleLanguage.dataset.language)
 })
